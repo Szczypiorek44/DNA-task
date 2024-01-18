6aab99a6f496c0407ae84b7acf5d705c0d48b220
Karol Michalski <karol.michalski95@gmail.com>
2024-01-18 11:10:19 +0100

    Reorganize ProductsView composable function

--- modules_application/presentation/src/main/java/io/dnatask/presentation/ProductsActivity.kt
+++ modules_application/presentation/src/main/java/io/dnatask/presentation/ProductsActivity.kt
@@ -3,38 +3,14 @@ package io.dnatask.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import io.dnatask.presentation.theme.Black
 import io.dnatask.presentation.theme.DNATaskAndroidTheme
 import io.dnatask.presentation.theme.MainBackground
-import io.dnatask.presentation.theme.White
-import io.dnatask.presentation.viewmodel.ProductsViewModel
-import io.dnatechnology.dnataskandroid.R
+import io.dnatask.presentation.ui.ProductsRoute
 
 class ProductsActivity : ComponentActivity() {
-
-    private val productsViewModel: ProductsViewModel by viewModels<ProductsViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,75 +20,15 @@ class ProductsActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MainBackground
                 ) {
-                    ProductsView(productsViewModel = productsViewModel)
+                    ProductsRoute()
                 }
             }
         }
     }
 }
 
-@Composable
-fun ProductsView(productsViewModel: ProductsViewModel) {
-    DNATaskAndroidTheme {
-        LaunchedEffect(Unit) {
-            productsViewModel.getProducts()
-        }
-
-        val products = productsViewModel.products.collectAsState().value
-
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.SpaceBetween,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            if (products != null) {
-                Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
-                    for (product in products) {
-                        Row(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(horizontal = 16.dp)
-                                .padding(bottom = 8.dp)
-                                .background(White)
-                                .border(1.dp, Black)
-                                .clickable {
-                                    productsViewModel.addToCart(product.productID)
-                                }
-                        ) {
-                            Text(
-                                text = product.toString(),
-                                color = Black,
-                                fontSize = 16.sp,
-                                modifier = Modifier
-                                    .padding(horizontal = 8.dp)
-                                    .padding(vertical = 8.dp)
-                            )
-                        }
-                    }
-                }
-            } else {
-                Text(text = "LOADING")
-            }
 
 
-            Row(
-                Modifier
-                    .background(White)
-                    .fillMaxWidth()
-                    .height(50.dp),
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(text = stringResource(R.string.pay), color = Black)
-            }
-        }
-    }
-}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DNATaskAndroidTheme {
-        ProductsView(productsViewModel = ProductsViewModel())
-    }
-}
+
+
---
+++ modules_application/presentation/src/main/java/io/dnatask/presentation/ui/ProductsScreen.kt
@@ -0,0 +1,124 @@
+package io.dnatask.presentation.ui
+
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import io.dnatask.data.Product
+import io.dnatask.presentation.ProductsActivity
+import io.dnatask.presentation.theme.Black
+import io.dnatask.presentation.theme.DNATaskAndroidTheme
+import io.dnatask.presentation.theme.White
+import io.dnatask.presentation.viewmodel.ProductsViewModel
+import io.dnatechnology.dnataskandroid.R
+
+@Composable
+fun ProductsRoute(productsViewModel: ProductsViewModel = viewModel()) {
+    LaunchedEffect(Unit) {
+        productsViewModel.getProducts()
+    }
+
+    val products by productsViewModel.products.collectAsStateWithLifecycle()
+
+    ProductsScreen(
+        products,
+        onProductClicked = { productsViewModel.addToCart(it.productID) })
+}
+
+@Composable
+fun ProductsScreen(products: List<Product>?, onProductClicked: (Product) -> Unit) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        if (!products.isNullOrEmpty()) {
+            LazyColumn(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+                items(products) {
+                    ProductRow(
+                        product = it,
+                        onClicked = onProductClicked
+                    )
+                }
+            }
+        } else {
+            Text(text = stringResource(R.string.loading))
+        }
+
+        PayButton(onPayButtonClicked())
+    }
+}
+
+@Composable
+private fun ProductRow(product: Product, onClicked: (Product) -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+            .padding(bottom = 8.dp)
+            .background(White)
+            .border(1.dp, Black)
+            .clickable(onClick = { onClicked.invoke(product) })
+    ) {
+        Text(
+            text = product.toString(),
+            color = Black,
+            fontSize = 16.sp,
+            modifier = Modifier
+                .padding(horizontal = 8.dp)
+                .padding(vertical = 8.dp)
+        )
+    }
+}
+
+@Composable
+private fun PayButton(onClicked: () -> Unit) {
+    Button(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight(),
+        colors = ButtonDefaults.buttonColors(White),
+        onClick = onClicked,
+    ) {
+        Text(
+            text = stringResource(R.string.pay),
+            color = Black,
+            modifier = Modifier.padding(5.dp)
+        )
+    }
+}
+
+private fun onPayButtonClicked(): () -> Unit = {
+    Log.d(ProductsActivity.TAG, "onPayButtonClicked()")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ProductsScreenPreview() {
+    DNATaskAndroidTheme {
+        ProductsRoute()
+    }
+}
