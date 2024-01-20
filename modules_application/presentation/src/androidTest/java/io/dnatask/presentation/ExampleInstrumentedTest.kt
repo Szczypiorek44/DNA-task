e4fb0073bad6c895884f4e6ed46459dd6c6710cc
Karol Michalski <karol.michalski95@gmail.com>
2024-01-20 11:32:49 +0100

    Adjust package for ExampleInstrumentedTest

--- modules_application/presentation/src/androidTest/java/io/dnatask/presentation/ExampleInstrumentedTest.kt
+++ modules_application/presentation/src/androidTest/java/io/dnatask/presentation/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package io.dnatechnology.dnataskandroid
+package io.dnatask.presentation
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("io.dnatechnology.dnataskandroid", appContext.packageName)
+        assertEquals("io.dnatask.presentation", appContext.packageName)
     }
 }
